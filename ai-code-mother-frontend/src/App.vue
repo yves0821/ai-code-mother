536e1fa3660713eed404471524c9c7e571cb1267
uncommon <649402073@qq.com>
2026-08-01 19:35:02 +0800

    用户模块前端模块完成 1. 开发用户登录页面 2. 开发用户注册页面 3. 引入全局状态管理，获取登录用户信息 4. 开发用户注销功能 5. 开发用户管理页面 6. 统一权限控制

--- ai-code-mother-frontend/src/App.vue
+++ ai-code-mother-frontend/src/App.vue
@@ -2,6 +2,10 @@
 import BasicLayout from '@/layouts/BasicLayout.vue'
 
 import { healthCheck } from '@/api/healthController.ts'
+import { useLoginUserStore } from '@/stores/loginUser.ts'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
 
 healthCheck().then((res) => {
   console.log(res)
