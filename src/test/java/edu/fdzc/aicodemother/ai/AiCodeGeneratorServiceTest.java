39d091877b11efdac2ff73f5ad2df53e54d15182
uncommon <649402073@qq.com>
2026-08-02 18:10:49 +0800

    AI应用生成功能完成 1. 接入 LangChain4j，实现 AI 对话服务 2. 开发文件保存器和代码解析器、AI 生成门面，实现 AI 生成网站文件 3. SSE 流式输出和解析

---
+++ src/test/java/edu/fdzc/aicodemother/ai/AiCodeGeneratorServiceTest.java
@@ -0,0 +1,28 @@
+package edu.fdzc.aicodemother.ai;
+
+import edu.fdzc.aicodemother.ai.model.HtmlCodeResult;
+import edu.fdzc.aicodemother.ai.model.MultiFileCodeResult;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个程序员的工作记录小工具");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult multiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个程序员的留言板");
+        Assertions.assertNotNull(multiFileCode);
+    }
+
+}
---
+++ src/test/java/edu/fdzc/aicodemother/core/AiCodeGeneratorFacadeTest.java
@@ -0,0 +1,36 @@
+package edu.fdzc.aicodemother.core;
+
+import edu.fdzc.aicodemother.model.enums.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+
+import java.io.File;
+import java.util.List;
+
+@SpringBootTest
+class AiCodeGeneratorFacadeTest {
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+    @Test
+    void generateAndSaveCode() {
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("任务记录网站", CodeGenTypeEnum.MULTI_FILE);
+        Assertions.assertNotNull(file);
+    }
+
+    @Test
+    void generateAndSaveCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream("任务记录网站", CodeGenTypeEnum.MULTI_FILE);
+        // 阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 验证结果
+        Assertions.assertNotNull(result);
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
+    }
+
+}
