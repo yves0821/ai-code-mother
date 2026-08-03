6db826860a3546ade6e4218e4dd3ba20e47fc5d4
uncommon <649402073@qq.com>
2026-08-03 17:44:54 +0800

    后端基础应用能力完成 【用户】 创建应用 根据 id 修改自己的应用 根据 id 删除自己的应用 根据 id 查看应用详情 分页查询自己的应用列表 分页查询精选的应用列表 【管理员】 根据 id 删除任意应用 根据 id 更新任意应用 分页查询应用列表 根据 id 查看应用详情

---
+++ src/main/java/edu/fdzc/aicodemother/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package edu.fdzc.aicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import edu.fdzc.aicodemother.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author YFF
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
